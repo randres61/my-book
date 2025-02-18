1a092f156323f13344c81e4255a5e957115fbb3a
randres61 <ramon.andres61@gmail.com>
2025-02-18 16:35:45 +0100

    Before add Routing

---
+++ src/book-list.js
@@ -0,0 +1,5 @@
+import {bindable} from 'aurelia';
+
+export class BookList {
+  @bindable books;
+}
--- src/my-app.js
+++ src/my-app.js
@@ -1,3 +1,23 @@
+import { inject } from 'aurelia';
+import { BookApi } from './book-api';
+
+@inject(BookApi)
 export class MyApp {
-  message = 'Hello World!';
+
+  constructor(bookApi){
+    this.bookTitle = "";
+    this.books = [];
+    this.bookApi = bookApi;
+  }
+
+  addBook () {
+    this.books.push({title : this.bookTitle});
+    this.bookTitle = "";
+  }
+
+  binding(){
+    this.bookApi.getBooks().then(savedBooks =>
+                       this.books = savedBooks);
+  }
 }
+
